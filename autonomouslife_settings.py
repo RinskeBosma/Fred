683d1db501504b811073bef76ddf937672cd7088
RinskeBosma <rinskebosma@gmail.com>
2017-11-18 17:54:32 +0100

    beschrijving erbij

--- autonomouslife_settings.py
+++ autonomouslife_settings.py
@@ -2,5 +2,8 @@ from naoqi import ALProxy
 from naoip import NAOIP
 autolife = ALProxy("ALAutonomousLife", NAOIP, 9559)
 print autolife.getState()
-autolife.setState("disabled")
+
+autolife.setState("solitary")
 print autolife.getState()
+
+#### the autonomous life states are: solitary, interactive, disabled, safeguard
