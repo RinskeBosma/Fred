b88b5a452ddc0a6493929538a14a2a809d198224
RinskeBosma <rinskebosma@gmail.com>
2017-11-18 16:36:21 +0100

    NAO ip from separate file

--- speak.py
+++ speak.py
@@ -1,3 +1,4 @@
 from naoqi import ALProxy
-tts = ALProxy("ALTextToSpeech", "169.254.133.122", 9559)
-tts.say("Dag, ik ben Fred van het Rosa Ensemble")
+from NaoIp import NAOIP
+tts = ALProxy("ALTextToSpeech", NAOIP, 9559)
+tts.say("Dag oma")
