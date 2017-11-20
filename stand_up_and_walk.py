be1b0d736a043b0889af3cb06738624041479756
RinskeBosma <rinskebosma@gmail.com>
2017-11-20 16:36:39 +0100

    uitleg

--- stand_up_and_walk.py
+++ stand_up_and_walk.py
@@ -16,9 +16,13 @@ motionProxy  = ALProxy("ALMotion", NAOIP, 9559)
 postureProxy.goToPosture("StandInit", 1.0)
 
 x  = 0
-y  = 0.9
-theta  = 0
+y  = 0
+theta  = math.pi/2
 motionProxy.moveTo(x, y, theta)
+
+### x = vooruit
+### y = naar links stappen
+### theta = om je as draaien
 
 
 
