d8ca8350cf58d44dc220343a7d66d50c73acd3d2
RinskeBosma <rinskebosma@gmail.com>
2017-11-24 19:15:39 +0100

    poging met dialog te openen met behaviorManager

--- open_dialog.py
+++ open_dialog.py
@@ -12,14 +12,9 @@ behaviorProxy = ALProxy("ALBehaviorManager", NAOIP, 9559)
 
 
 
-print dialogProxy.getUserList()
+# print behaviorProxy.getDefaultBehaviors()
+# print behaviorProxy.getInstalledBehaviors()
+# print behaviorProxy.getLoadedBehaviors()
 
-print dialogProxy.getLoadedTopics("English")
-dialogProxy.activateTopic('dlg_how_are_you')
-
-print dialogProxy.getActivatedTopics
-
-dialogProxy.gotoTopic('dlg_how_are_you')
-
-
-dialogProxy.forceOutput()
+behaviorProxy.startBehavior("")
+print behaviorProxy.getRunningBehaviors()
